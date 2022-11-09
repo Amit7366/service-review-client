98c075746e1ca088a2ef5cfd1d83f77fa07d869c
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-10 00:34:37 +0600

    load servie from db to home page

---
+++ src/Components/Services/ServiceCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import { Badge,Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { FaStar } from 'react-icons/fa';
+
+const ServiceCard = ({service}) => {
+    const {_id,title,image,price,ratings,details} = service;
+    return (
+        <div className="w-full rounded-md shadow-md flex flex-col pb-4">
+          <div>
+            <PhotoProvider>
+              <PhotoView src={image}>
+                <img
+                  src={image}
+                  alt=""
+                  className="rounded-t-md h-64 w-full object-cover"
+                />
+              </PhotoView>
+            </PhotoProvider>
+          </div>
+          <div className="px-4 pt-4">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {title}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {details.slice(0,100)}
+            </p>
+            <div className="flex flex-wrap gap-2 my-4">
+              <Badge color="failure" size="sm">
+                ${price}
+              </Badge>
+              <div className='flex items-center gap-1 px-2 bg-teal-400 rounded-sm text-white'><FaStar></FaStar> {ratings}</div>
+            </div>
+            <Link to="/services/1">
+              <Button gradientMonochrome="purple">View Details</Button>
+            </Link>
+          </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,111 +1,26 @@
-import { Badge, Button } from "flowbite-react";
-import React from "react";
+import { Button } from "flowbite-react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { PhotoProvider, PhotoView } from "react-photo-view";
+import ServiceCard from "./ServiceCard";
+
 
 const Services = () => {
+  const [services,setServices] = useState([]);
+
+  useEffect(() =>{
+    fetch('http://localhost:5000/services')
+    .then(res => res.json())
+    .then(data => setServices(data))
+  },[])
+
   return (
     <div>
       <div className="mx-auto gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10">
-        <div className="w-full rounded-md shadow-md flex flex-col pb-4">
-          <div>
-            <PhotoProvider>
-              <PhotoView src="https://flowbite.com/docs/images/blog/image-1.jpg">
-                <img
-                  src="https://flowbite.com/docs/images/blog/image-1.jpg"
-                  alt=""
-                  className="rounded-t-md"
-                />
-              </PhotoView>
-            </PhotoProvider>
-          </div>
-          <div className="px-4 pt-4">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <div className="flex flex-wrap gap-2 my-4">
-              <Badge color="failure" size="sm">
-                Failure
-              </Badge>
-              <Badge color="success" size="sm">
-                Success
-              </Badge>
-            </div>
-            <Link to="/services/1">
-              <Button gradientMonochrome="purple">View Details</Button>
-            </Link>
-          </div>
-        </div>
-        <div className="w-full rounded-md shadow-md flex flex-col pb-4">
-          <div>
-            <PhotoProvider>
-              <PhotoView src="https://flowbite.com/docs/images/blog/image-1.jpg">
-                <img
-                  src="https://flowbite.com/docs/images/blog/image-1.jpg"
-                  alt=""
-                  className="rounded-t-md"
-                />
-              </PhotoView>
-            </PhotoProvider>
-          </div>
-          <div className="px-4 pt-4">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <div className="flex flex-wrap gap-2 my-4">
-              <Badge color="failure" size="sm">
-                Failure
-              </Badge>
-              <Badge color="success" size="sm">
-                Success
-              </Badge>
-            </div>
-            <Link to="/services/1">
-              <Button gradientMonochrome="purple">View Details</Button>
-            </Link>
-          </div>
-        </div>
-        <div className="w-full rounded-md shadow-md flex flex-col pb-4">
-          <div>
-            <PhotoProvider>
-              <PhotoView src="https://flowbite.com/docs/images/blog/image-1.jpg">
-                <img
-                  src="https://flowbite.com/docs/images/blog/image-1.jpg"
-                  alt=""
-                  className="rounded-t-md"
-                />
-              </PhotoView>
-            </PhotoProvider>
-          </div>
-          <div className="px-4 pt-4">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <div className="flex flex-wrap gap-2 my-4">
-              <Badge color="failure" size="sm">
-                Failure
-              </Badge>
-              <Badge color="success" size="sm">
-                Success
-              </Badge>
-            </div>
-            <Link to="/services/1">
-              <Button gradientMonochrome="purple">View Details</Button>
-            </Link>
-          </div>
-        </div>
+        
+        {
+          services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+        }
+
 
       </div>
       <div className="w-full text-center">
